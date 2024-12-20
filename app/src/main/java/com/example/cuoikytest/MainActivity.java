ecfb3c273f64dc15c15fd3dcfcf0c0b3919c8656
PhanMinhNhat0601 <nhatphan612004@gmail.com>
2024-12-20 21:05:24 +0700

    Commit Nhat

--- app/src/main/java/com/example/cuoikytest/MainActivity.java
+++ app/src/main/java/com/example/cuoikytest/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String nhat1, nhat2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
