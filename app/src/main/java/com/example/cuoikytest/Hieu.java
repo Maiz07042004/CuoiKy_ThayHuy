ccd7a2fbeb21de19be0e8a41465b95ff7cd5e9d7
Hieuiutatcamn <conghieu2k4@gmail.com>
2024-12-20 21:14:59 +0700

    Activity hieu

---
+++ app/src/main/java/com/example/cuoikytest/Hieu.java
@@ -0,0 +1,4 @@
+package com.example.cuoikytest;
+
+public class Hieu {
+}
--- app/src/main/java/com/example/cuoikytest/MainActivity.java
+++ app/src/main/java/com/example/cuoikytest/MainActivity.java
@@ -11,8 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private String nhat1, nhat2;
-
-        private String hieu1,hieu2;
+    private String hieu1,hieu2;
     private String mai1;
     private String mai2;
 
